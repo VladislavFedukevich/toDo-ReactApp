bb3c0c040f34542a83e9cb9389adee0aa5338c1a
karmanchik567 <fedukevicvlad@gmail.com>
2023-02-04 03:28:00 +0300

    add toDoList

--- toDo-reactApp/src/App.tsx
+++ toDo-reactApp/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import { IconButton } from '@chakra-ui/button'
+import { Heading, useColorMode, VStack} from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import './App.css';
+import AddTodo from './components/AddTodo';
 import ToDoList from './components/ToDoList';
+import useTodo from './hooks/useTodo';
 
 const App = () => {
   const [todos, addTodo, deleteTodo] = useTodo();
+  const { colorMode, toggleColorMode} = useColorMode();
+
+  const buttonProps = {
+    size: 'lg',
+    isRound: true,
+    alignSelf: 'flex-end',
+    icon: colorMode === 'light' ? <FaMoon /> : <FaSun />,
+    'aria-label': 'Switch DarkMode',
+  }
+
   return (
-    <div>
-      <h1>My ToDo App!</h1>
-      <ToDoList />
-    </div>
+    <VStack p={4}>
+      <IconButton onClick={toggleColorMode} {...buttonProps} />
+      <Heading size='2xl'>To-Do React-TypeScript-ChakraUI</Heading>
+      <AddTodo addTodo={addTodo} />
+      <ToDoList todos={todos} deleteTodo={deleteTodo} />
+    </VStack>
   )
 }
 
--- toDo-reactApp/src/main.tsx
+++ toDo-reactApp/src/main.tsx
@@ -1,3 +1,5 @@
+import { ColorModeScript } from '@chakra-ui/color-mode'
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -5,6 +7,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode='light' />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
